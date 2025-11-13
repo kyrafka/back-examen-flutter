a622139bce6ed9c562a98599caae9ae3092d1b9a
Jose Zuñiga <josedario.zuniga@gmail.com>
2025-11-13 13:04:12 -0600

    EXAMEN

---
+++ src/test/java/com/example/backexamen/BackExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
